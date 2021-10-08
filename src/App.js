7d86dee14a6eb37dee42f724bfb01aa2fb3808e3
therman mcmillan <mcmillanj0106@yahoo.com>
2021-10-08 13:44:48 -0400

    mvp met all passed done

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
+import axios from 'axios'
+import Character  from './components/Character'
 import './App.css';
 
 const App = () => {
@@ -8,10 +10,22 @@ const App = () => {
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  const [character, setCharacter]= useState([])
+  useEffect(() => {
+  axios.get('https://swapi.dev/api/people/')
+    .then(response => {
+    setCharacter(response.data)
+  })
+  .catch(error => {
+    console.log('error.there is a data issue')
+  })
+}, [])
+
 
   return (
     <div className="App">
       <h1 className="Header">Characters</h1>
+      <Character character={character}/>
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,55 @@
 // Write your Character component here
+import React,{useState} from 'react'
+import styled from 'styled-components';
+
+const stlyedContainer = styled.div`
+width:75%;
+border:2px solid black;
+`;
+const styledDiv = styled.div`
+border:2px solid black;
+background-color:black;
+
+`;
+const styledNameH2= styled.h2`
+font-size:24px;
+color:white;
+background-color:black;
+font-family:fantasy
+`;
+const styledInfo = styled.p`
+color: black;
+font-size:20px;
+font-family:fantasy
+`;
+
+function Character(props)
+{
+ const {character} = props;
+ 
+return(
+    <stlyedContainer classname='character'>
+        
+        {character.map((item) =>{
+
+           return( 
+            <styledDiv> 
+             <styledName>Name:{item.name}</styledName>
+             <styledInfo >  
+              <p>gender:{item.gender}</p>              
+             <p>Height:{item.height}</p>
+             <p>Mass:{item.mass}</p>
+             <p>Hair_color:{item.hair_color}</p>
+             <p>Skin_color:{item.skin_color}</p>
+             <p>Birth Year:{item.birth_year}</p>
+             </styledInfo >
+             </styledDiv>
+           )
+            })};
+             
+         </stlyedContainer>
+
+)
+
+}
+export default Character;
